1ebb661890c49d96c8c923c1c3c9e48bdb289889
iambhavesh <bhaveshrathod48@gmail.com>
2021-10-16 11:55:18 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [res, setres] = useState("");
+  const [display, setdisplay] = useState("");
+  const [equals, setequals] = useState(false)
+  const btnValues = [
+    ["AC", "Del", "/", "*"],
+    [7, 8, 9, "-"],
+    [4, 5, 6, "+"],
+    [1, 2, 3],
+    [0, ".", "="],
+  ];
+
+  const operations = ["+", "-", "*", "/", "."];
+
+  const onValuesClickHandler = (value) => {
+    if (
+      (operations.includes(value) && operations.includes(res.slice(-1))) ||
+      (operations.includes(value) && res === "")
+    ) {
+      return;
+    }
+    setres(res + value);
+    if (!operations.includes(value)) {
+      setdisplay(eval(res + value));
+    }
+  };
+
+  const resetHandler = (e) => {
+    e.preventDefault();
+    setres("");
+    setdisplay("");
+  };
+
+  const backspaceHandler = () => {
+    if (res === "" || equals=== true) {
+      return;
+    }
+    const value = res.slice(0, -1);
+    setres(value);
+  };
+
+  const evaluateHandler = () => {
+    setequals(true);
+    setres(eval(res));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="screen">
+        <p>{res ? res : 0}</p>
+        <p>{display ? "=" + display : 0}</p>
+      </div>
+      <div className="button-box">
+        {btnValues.flat().map((btn, i) => {
+          return (
+            <button
+              key={i}
+              className={
+                btn === "+"
+                  ? "plus"
+                  : btn === "="
+                  ? "equals"
+                  : btn === 0
+                  ? "zero"
+                  : btn === "/"
+                  ? "blue"
+                  : btn === "*"
+                  ? "blue"
+                  : btn === "-"
+                  ? "blue"
+                  : btn === "AC"
+                  ? "grey"
+                  : btn === "Del"
+                  ? "grey"
+                  : ""
+              }
+              onClick={
+                btn === "AC"
+                  ? resetHandler
+                  : btn === "Del"
+                  ? backspaceHandler
+                  : btn === "="
+                  ? evaluateHandler
+                  : () => onValuesClickHandler(btn)
+              }
+            >
+              {btn}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
